52d11c353606f0a4f18b50dc50373c0cab6ec5fa
Zooprojectvision <artworldconsulting@gmail.com>
2025-10-26 11:15:21 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
+import './i18n.js'
 
-const root = document.getElementById('root')
-createRoot(root).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
