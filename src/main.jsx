8d8dec2c8ed29a8eb882f5925153b2daf6eb5052
Zooprojectvision <artworldconsulting@gmail.com>
2025-10-29 01:00:11 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,2 @@
-// src/main.jsx
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+console.log('MAIN MONTÉ')
 
-import './styles.css'     // styles globaux (pas de background ici)
-import './theme-muso.css' // fond noir (optionnel, peut être vide si index.html gère le fond)
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
