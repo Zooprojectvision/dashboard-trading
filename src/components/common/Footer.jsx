98102386cb7a11740bfd77b2ab3d3cb64658bf11
Zooprojectvision <artworldconsulting@gmail.com>
2025-11-03 10:20:23 +0100

    Update Footer.jsx

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,24 +1,2 @@
-// src/components/common/Footer.jsx
-import React from "react";
-import { APP_VERSION } from "../../version";
-
-export default function Footer() {
-  return (
-    <div
-      className="footer"
-      style={{
-        textAlign: "center",
-        color: "var(--text)",
-        opacity: 0.7,
-        fontSize: 12,
-        marginTop: 20,
-      }}
-    >
-      Designed &amp; Built by ZooProjectVision V{APP_VERSION} @{" "}
-      {new Date().getFullYear()}
-    </div>
-  );
-}
-
 
 
